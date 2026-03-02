1cd5025769ab2ad03183ee84113a18e8f23f9c2c
MURARIDHAR PAL <78611514+hmwaio@users.noreply.github.com>
2026-03-02 07:50:28 +0530

    Update email service connection test function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config"
 import app from "./app.js";
-import { testConnection } from "./services/email/email.service.js";
+import { testBrevoConnection } from "./services/email/email.service.js";
 import { startCleanupJob } from "./jobs/cleanup.job.js";
 
 const PORT = process.env.PORT || 3000;
@@ -10,5 +10,5 @@ app.listen(PORT, async () => {
   console.log(`App is listening on http://localhost:${PORT}`);
   startCleanupJob();
 
-  await testConnection();
+  await testBrevoConnection();
 });
